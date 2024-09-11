1676ee487a18c30014de5b427e571a2aad89e1b9
IRO0w0 <156142836+IRO0w0@users.noreply.github.com>
2024-09-11 15:35:44 +0900

    SimpleButtonGame

--- Assets/ButtonEff.cs
+++ Assets/ButtonEff.cs
@@ -1,20 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class ButtonEff : MonoBehaviour
-{
-    public GameObject star;
-    public List<GameObject> btns;
-    // Start is called before the first frame update
-    void Start()
-    {
+//public class ButtonEff : MonoBehaviour
+//{
+//    public GameObject star;
+//    public List<GameObject> btns;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
-}
+//    }
+//}
--- Assets/StarEF.cs
+++ Assets/StarEF.cs
@@ -1,27 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // UI 네임스페이스를 추가해야 합니다.
 
 public class StarEF : MonoBehaviour
 {
-    public GameObject star;
-    private float spawnTime;
-    private float defaultTime = 0.1f;
- 
+    public GameObject star; // 생성할 별 오브젝트
+    public Button starButton; // UI 버튼
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        if (Input.GetMouseButton(0) && spawnTime >= defaultTime)
-        {
-            MakeStar();
-            spawnTime = 0;
-        }
-        spawnTime += Time.deltaTime;
-
+        // 버튼에 클릭 이벤트를 연결
+        starButton.onClick.AddListener(MakeStar);
     }
+
     void MakeStar()
     {
+        // 마우스 위치에 별 생성
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouse.z = 0;
         Instantiate(star, mouse, Quaternion.identity);
